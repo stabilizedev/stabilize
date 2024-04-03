7bc2af0130667b3d5fc3de87328e63f560ff5a19
Felipe Vega <felipevega@hey.com>
2024-04-03 14:49:26 -0700

    wip

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,200 @@
 @extends('_layouts.main')
 
 @section('body')
-<div class="p-8">
-    <h1 class="text-3xl font-bold">Hello world!</h1>
+<div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto lg:max-w-none">
+        <h1 class="text-5xl tracking-tight flex flex-col gap-y-2 font-medium">
+            <p>
+                <span
+                    class="bg-gradient-to-b from-emerald-300 to-emerald-800 text-transparent bg-clip-text font-semibold leading-10">Reduce</span>
+                Friction
+            </p>
+            <p>
+                <span
+                    class="bg-gradient-to-b from-emerald-300 to-emerald-800 text-transparent bg-clip-text font-semibold leading-10">Maximize</span>
+                Momentum
+            </p>
+        </h1>
+        <p class="mt-6 text-neutral-300">
+            We get your development moving, improve delays, and
+            keep you balanced. With <strong
+                class="font-medium">stability</strong> comes great
+            software!
+        </p>
+    </div>
+</div>
+
+<div class="py-12 sm:py-16">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto lg:text-center">
+            <h2 class="text-base font-semibold leading-7 text-emerald-600">
+                Start Moving</h2>
+            <p class="mt-2 text-3xl font-medium tracking-tight sm:text-4xl">
+                Strong ideas need a strong foundation</p>
+            <p class="mt-6 text-lg leading-8 text-gray-300">Quis tellus
+                eget adipiscing convallis sit sit eget aliquet quis.
+                Suspendisse eget egestas a elementum pulvinar et feugiat
+                blandit at. In mi viverra elit nunc.</p>
+        </div>
+        <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+            <dl
+                class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                <div class="relative pl-16">
+                    <dt class="text-base font-semibold leading-7">
+                        <div
+                            class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-emerald-600">
+                            <svg class="h-6 w-6 text-white" fill="none"
+                                viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
+                            </svg>
+                        </div>
+                        Push to deploy
+                    </dt>
+                    <dd class="mt-2 text-base leading-7 text-gray-300">
+                        Morbi viverra dui mi arcu sed. Tellus semper
+                        adipiscing suspendisse semper morbi. Odio urna
+                        massa nunc massa.</dd>
+                </div>
+                <div class="relative pl-16">
+                    <dt class="text-base font-semibold leading-7">
+                        <div
+                            class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-emerald-600">
+                            <svg class="h-6 w-6 text-white" fill="none"
+                                viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                            </svg>
+                        </div>
+                        SSL certificates
+                    </dt>
+                    <dd class="mt-2 text-base leading-7 text-gray-300">
+                        Sit quis amet rutrum tellus ullamcorper
+                        ultricies libero dolor eget. Sem sodales gravida
+                        quam turpis enim lacus amet.</dd>
+                </div>
+            </dl>
+        </div>
+    </div>
+</div>
+
+<div class="py-12 sm:py-16">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto lg:text-center">
+            <h2 class="text-base font-semibold leading-7 text-emerald-600">
+                Keep Going</h2>
+            <p class="mt-2 text-3xl font-medium tracking-tight sm:text-4xl">
+                Great process, even greater people</p>
+            <p class="mt-6 text-lg leading-8 text-gray-300">Quis tellus
+                eget adipiscing convallis sit sit eget aliquet quis.
+                Suspendisse eget egestas a elementum pulvinar et feugiat
+                blandit at. In mi viverra elit nunc.</p>
+        </div>
+        <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+            <dl
+                class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                <div class="relative pl-16">
+                    <dt class="text-base font-semibold leading-7">
+                        <div
+                            class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-emerald-600">
+                            <svg class="h-6 w-6 text-white" fill="none"
+                                viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
+                            </svg>
+                        </div>
+                        Push to deploy
+                    </dt>
+                    <dd class="mt-2 text-base leading-7 text-gray-300">
+                        Morbi viverra dui mi arcu sed. Tellus semper
+                        adipiscing suspendisse semper morbi. Odio urna
+                        massa nunc massa.</dd>
+                </div>
+                <div class="relative pl-16">
+                    <dt class="text-base font-semibold leading-7">
+                        <div
+                            class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-emerald-600">
+                            <svg class="h-6 w-6 text-white" fill="none"
+                                viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                            </svg>
+                        </div>
+                        SSL certificates
+                    </dt>
+                    <dd class="mt-2 text-base leading-7 text-gray-300">
+                        Sit quis amet rutrum tellus ullamcorper
+                        ultricies libero dolor eget. Sem sodales gravida
+                        quam turpis enim lacus amet.</dd>
+                </div>
+            </dl>
+        </div>
+    </div>
+</div>
+
+<div class="py-12 sm:py-16">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto lg:text-center">
+            <h2 class="text-base font-semibold leading-7 text-emerald-600">
+                Stay Balanced</h2>
+            <p class="mt-2 text-3xl font-medium tracking-tight sm:text-4xl">
+                Movement isn't everything</p>
+            <p class="mt-6 text-lg leading-8 text-gray-300">Quis tellus
+                eget adipiscing convallis sit sit eget aliquet quis.
+                Suspendisse eget egestas a elementum pulvinar et feugiat
+                blandit at. In mi viverra elit nunc.</p>
+        </div>
+        <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+            <dl
+                class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                <div class="relative pl-16">
+                    <dt class="text-base font-semibold leading-7">
+                        <div
+                            class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-emerald-600">
+                            <svg class="h-6 w-6 text-white" fill="none"
+                                viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
+                            </svg>
+                        </div>
+                        Push to deploy
+                    </dt>
+                    <dd class="mt-2 text-base leading-7 text-gray-300">
+                        Morbi viverra dui mi arcu sed. Tellus semper
+                        adipiscing suspendisse semper morbi. Odio urna
+                        massa nunc massa.</dd>
+                </div>
+                <div class="relative pl-16">
+                    <dt class="text-base font-semibold leading-7">
+                        <div
+                            class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-emerald-600">
+                            <svg class="h-6 w-6 text-white" fill="none"
+                                viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                            </svg>
+                        </div>
+                        SSL certificates
+                    </dt>
+                    <dd class="mt-2 text-base leading-7 text-gray-300">
+                        Sit quis amet rutrum tellus ullamcorper
+                        ultricies libero dolor eget. Sem sodales gravida
+                        quam turpis enim lacus amet.</dd>
+                </div>
+            </dl>
+        </div>
+    </div>
 </div>
 @endsection
