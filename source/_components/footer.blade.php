daaf1103c2c4e88f01b88b4774a676087d19443b
Felipe Vega <felipevega@hey.com>
2024-04-25 12:04:40 -0700

    update form and footer

--- source/_components/footer.blade.php
+++ source/_components/footer.blade.php
@@ -28,15 +28,17 @@
                 class="text-xs sm:text-base font-medium leading-5 text-neutral-50">
                 &copy; {{ date('Y') }} Stabilize Development</p>
             <div class="flex space-x-3 sm:space-x-6">
-                <a href="#" class="text-neutral-50 hover:text-neutral-400">
-                    <span class="sr-only">X</span>
+                <a href="https://twitter.com/stabilizedev"
+                    class="text-neutral-50 hover:text-neutral-400">
+                    <span class="sr-only">Twitter</span>
                     <svg class="h-5 w-5 sm:h-6 sm:w-6 fill-current text-neutral-50"
                         viewBox="0 0 24 24" aria-hidden="true">
                         <path
                             d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
                     </svg>
                 </a>
-                <a href="#" class="text-neutral-50 hover:text-neutral-400">
+                <a href="https://github.com/stabilizedev"
+                    class="text-neutral-50 hover:text-neutral-400">
                     <span class="sr-only">GitHub</span>
                     <svg class="h-5 w-5 sm:h-6 sm:w-6 fill-current text-neutral-50"
                         viewBox="0 0 24 24" aria-hidden="true">
@@ -45,8 +47,9 @@
                             clip-rule="evenodd" />
                     </svg>
                 </a>
-                <a href="#" class="text-neutral-50 hover:text-neutral-400">
-                    <span class="sr-only">GitHub</span>
+                <a href="https://www.linkedin.com/company/stabilizedev"
+                    class="text-neutral-50 hover:text-neutral-400">
+                    <span class="sr-only">LinkedIn</span>
                     <svg class="h-5 w-5 sm:h-6 sm:w-6 fill-current text-neutral-50"
                         viewBox="0 0 24 24" aria-hidden="true">
                         <path fill-rule="evenodd"
