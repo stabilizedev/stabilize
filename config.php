0e9fbf0a789d64d01a7a17620cc8c8d18cfb3096
Felipe Vega <felipevega@hey.com>
2024-04-24 19:37:57 -0700

    add og data

--- config.php
+++ config.php
@@ -2,8 +2,9 @@
 
 return [
     'production' => false,
-    'baseUrl' => '',
-    'title' => 'Stabilize',
-    'description' => 'Website description.',
+    'baseUrl' => 'http://stabilize.test',
+    'title' => 'Stabilize - Forget Friction, Maximize Momentum',
+    'description' => 'Stabilize is a software development agency focused on stable web application development with Laravel.',
+    'image' => '/assets/images/stabilize-og-image.png',
     'collections' => [],
 ];
