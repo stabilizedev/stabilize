2528ae003a860b7c7d3939ca51edfcb61149faff
Felipe Vega <felipevega@hey.com>
2024-05-01 21:33:32 -0700

    add team section placeholder

--- source/index.blade.php
+++ source/index.blade.php
@@ -279,6 +279,102 @@
     </div>
 </div>
 
+<div class="py-20 sm:py-24 lg:py-32" id="services">
+    <div class="mx-auto max-w-screen-2xl px-6 lg:px-12 xl:px-24">
+        <div
+            class="mx-auto grid max-w-screen-2xl grid-cols-1 gap-x-8 gap-y-20 lg:grid-cols-3">
+            <div class="mx-auto max-w-2xl lg:mx-0">
+                <h2 class="text-base font-semibold leading-7 text-emerald-500">
+                    Passionate Programmers</h2>
+                <p
+                    class="mt-2 text-3xl font-bold tracking-tight text-neutral-100 sm:text-4xl">
+                    Meet the Team</p>
+                <p class="mt-6 text-base leading-7 text-neutral-300">Lorem ipsum
+                    dolor sit amet consectetur adipisicing elit. Reprehenderit
+                    distinctio illum optio sequi cumque quisquam itaque
+                    molestiae
+                    fuga, reiciendis possimus porro officiis voluptas
+                    temporibus,
+                    doloremque accusamus aliquid, eum dolores placeat.</p>
+            </div>
+            <ul role="list"
+                class="mx-auto grid max-w-2xl grid-cols-2 gap-x-6 gap-y-20 lg:mx-0 lg:max-w-none lg:gap-x-8 lg:col-span-2 lg:grid-cols-3">
+                <li>
+                    <img class="aspect-square w-full rounded-2xl object-cover"
+                        src="/assets/images/headshot-formal.JPEG" alt="">
+                    <h3
+                        class="mt-6 text-lg font-semibold leading-8 text-neutral-50">
+                        Felipe Vega</h3>
+                    <p class="text-base leading-7 text-emerald-500">
+                        Principal Programmer & Founder
+                    </p>
+                    <ul role="list" class="mt-6 flex gap-x-6">
+                        <li>
+                            <a href="#"
+                                class="text-emerald-500 hover:text-emerald-600">
+                                <span class="sr-only">X</span>
+                                <svg class="h-5 w-5" fill="currentColor"
+                                    viewBox="0 0 20 20" aria-hidden="true">
+                                    <path
+                                        d="M11.4678 8.77491L17.2961 2H15.915L10.8543 7.88256L6.81232 2H2.15039L8.26263 10.8955L2.15039 18H3.53159L8.87581 11.7878L13.1444 18H17.8063L11.4675 8.77491H11.4678ZM9.57608 10.9738L8.95678 10.0881L4.02925 3.03974H6.15068L10.1273 8.72795L10.7466 9.61374L15.9156 17.0075H13.7942L9.57608 10.9742V10.9738Z" />
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="text-emerald-500 hover:text-emerald-600">
+                                <span class="sr-only">LinkedIn</span>
+                                <svg class="h-5 w-5" fill="currentColor"
+                                    viewBox="0 0 20 20" aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                        d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <img class="aspect-square w-full rounded-2xl object-cover"
+                        src="https://media.licdn.com/dms/image/C5603AQE40s-RtXyvNg/profile-displayphoto-shrink_800_800/0/1562631781431?e=1719446400&v=beta&t=Ld-vnJEopmVvRkd7-rfA7R4Z1S192EqRiiyNixJk8xQ"
+                        alt="">
+                    <h3
+                        class="mt-6 text-lg font-semibold leading-8 text-neutral-50">
+                        Michael Diaz</h3>
+                    <p class="text-base leading-7 text-emerald-500">
+                        Product Manager & Founder
+                    </p>
+                    <ul role="list" class="mt-6 flex gap-x-6">
+                        <li>
+                            <a href="#"
+                                class="text-emerald-500 hover:text-emerald-600">
+                                <span class="sr-only">X</span>
+                                <svg class="h-5 w-5" fill="currentColor"
+                                    viewBox="0 0 20 20" aria-hidden="true">
+                                    <path
+                                        d="M11.4678 8.77491L17.2961 2H15.915L10.8543 7.88256L6.81232 2H2.15039L8.26263 10.8955L2.15039 18H3.53159L8.87581 11.7878L13.1444 18H17.8063L11.4675 8.77491H11.4678ZM9.57608 10.9738L8.95678 10.0881L4.02925 3.03974H6.15068L10.1273 8.72795L10.7466 9.61374L15.9156 17.0075H13.7942L9.57608 10.9742V10.9738Z" />
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="text-emerald-500 hover:text-emerald-600">
+                                <span class="sr-only">LinkedIn</span>
+                                <svg class="h-5 w-5" fill="currentColor"
+                                    viewBox="0 0 20 20" aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                        d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
 <div class="py-20 sm:py-24 lg:py-32" id="methods">
     <div class="mx-auto max-w-screen-2xl px-6 lg:px-12 xl:px-24">
         <div
